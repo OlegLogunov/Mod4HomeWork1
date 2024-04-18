f21f3e590213cc546143c5c4a9c9160480620686
OlegLogunov <164496716+OlegLogunov@users.noreply.github.com>
2024-04-18 13:00:40 +0300

    Add files via upload

---
+++ HomeWork4Mod1.py
@@ -0,0 +1,6 @@
+def func1():
+    print("Hello, world")
+
+def func2():
+    print("Hello, world")
+
---
+++ main.py
@@ -0,0 +1,9 @@
+# 1 Способ
+import HomeWork4Mod1
+HomeWork4Mod1.func1()
+HomeWork4Mod1.func2()
+
+# 2 Способ
+from HomeWork4Mod1 import func1, func2
+func1()
+func2()
